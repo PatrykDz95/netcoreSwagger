004789eb202ff9ec1d8c875ec7ed614e33fcec94
Patryk <patrykdziedzioch@gmail.com>
2018-12-16 14:11:14 +0100

    Saving changes

---
+++ netCore/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using netCore.Models;
+using netCore.Controllers;
+using netCore.Persistance;
+
+namespace netCore.Controllers
+{
+    [Route("api/[controller]")]
+    public class HomeController : Controller
+    {
+        private UnitOfWork unitOfWork = new UnitOfWork();
+
+        //GET api/values
+        [HttpGet]
+       public JsonResult Get()
+        {
+            var product = unitOfWork.ProductRepository.GetAllProducts();
+            return Json(product.ToList());
+        }
+
+        //GET api/values/id
+        [HttpGet("id")]
+        public Product GetById(int id)
+        {
+            Product product = unitOfWork.ProductRepository.GetProduct(id);
+            return product;
+        }
+
+        //POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        //PUT api/values/id
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+           
+        }
+    }
+}
---
+++ netCore/Models/ProductContext.cs
@@ -0,0 +1,24 @@
+﻿using netCore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace netCore.Models
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext()
+        {
+        }
+
+        public ProductContext(DbContextOptions<ProductContext> options)
+            : base(options)
+        { }      
+        
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Feature> Features { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           
+        }
+    }   
+}
